d05b3efedbf7a45ba61bd24851412e323954905e
Farros <86004023+farroshsy@users.noreply.github.com>
2022-04-10 15:39:11 +0700

    Add files via upload

--- Soal3.R
+++ Soal3.R
@@ -1,32 +1,32 @@
-# Soal 3a
-# Berapa peluang bahwa 6 bayi akan lahir di rumah sakit ini besok?
-# Diketahui average: 4.5 bayi, x = 6 bayi
-dpois(6, 4.5)
+# Poin 3a
+bayi_lahir = 6
+rata_rata_bayi_lahir = 4.5
+dpois(bayi_lahir, rata_rata_bayi_lahir)
 
-# Soal 3b
-# Berapa peluang bahwa 6 bayi akan lahir di rumah sakit ini besok?
+# Poin 3b
+n = 365
+rata_rata_bayi_lahir = 4.5
 set.seed(2)
-babies <- data.frame('data' = rpois(365, 4.5))
+babies <- data.frame('data' = rpois(n, rata_rata_bayi_lahir))
 
 babies %>% ggplot() +
   geom_histogram(aes(x = data, y = stat(count / sum(count)), fill = data == 6), binwidth = 1, color = 'black',) +
-  scale_fill_manual(values = c("#FFCC66", "#669933")) +
+  scale_fill_manual(values = c("#6BCB77", "#FF6B6B")) +
   scale_x_continuous(breaks = 0:10) +
-  labs(x = 'Jumlah bayi lahir per periode', y = 'Proporsi', title = 'Simulasi kelahiran selama setahun (n=365) di rumah sakit') +
+  labs(x = 'Jumlah bayi lahir per periode', y = 'Proporsi', title = 'Simulasi kelahiran selama setahun (n = 365)') +
   theme_bw()
 
-# Soal 3c
-# dan bandingkan hasil poin a dan b , Apa kesimpulan yang bisa didapatkan
+
+# Poin 3c
 # Perbandingan poin a dan poin b kurang lebih sama karena nilai proporsi yang didapatkan di poin A 
 # berada dalam range proporsi yang ada di poin B yaitu 0.10-0.15. Oleh karena itu, pengaruh perhitungan 
 # dengan estimasi selama 365 hari memberikan hasil yang mendekati dengan peluang bayi akan dilahirkan keesokan harinya
 
-# Soal 3d
-# Inisialiasi
-l <- 6
-# Mencari nilai rata-rata
+# Poin 3d
+bayi_lahir = 6
+l <- bayi_lahir
+
 rataan <- l
 rataan
-# Mencari nilai varian
 varian <- l
 varian
