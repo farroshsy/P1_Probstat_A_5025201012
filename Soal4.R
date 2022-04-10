5eb9b5d60aa09cebbf531c83be4a26c9578ce559
Farros <86004023+farroshsy@users.noreply.github.com>
2022-04-10 15:47:06 +0700

    Add files via upload

--- Soal4.R
+++ Soal4.R
@@ -1,20 +1,32 @@
-# Soal 4a
-# Fungsi Probabilitas dari Distribusi Chi-Square
-x <- 2
-v <- 10
+# Poin 4a
+x = 2
+v = 10
 dchisq(x, v)
 
-# Soal 4b
-# Histogram dari Distribusi Chi-Square dengan 100 data random
-# Melakukan random data
-hist(rchisq(100, v))
+# Poin 4b
+random_data = 100
+hist(rchisq(random_data, v))
 
 # Soal 4c
-# Inisialiasi
-df <- 10
-# Mencari nilai rata-rata
-rataan <- df
+d = 10
+rataan <- d
 rataan
-# Mencari nilai varian
-varian <- 2 * df
+varian <- 2 * d
 varian
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
